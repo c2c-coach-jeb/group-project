9064680d1a90ef0fe96ade8bab2c5159c6ce5588
Omari Middleton <omarimiddlet007@gmail.com>
2024-10-28 23:12:10 +0000

    change

--- test_isprime.py
+++ test_isprime.py
@@ -5,13 +5,13 @@ import problem2 as primes
 class TestIsPrime(unittest.TestCase):
 
     def test_two_is_prime(self):
-        self.assertTrue(primes.is_prime(2))
+        self.assertTrue(primes.is_prime(1))
 
     def test_three_is_prime(self):
-        self.assertTrue(primes.is_prime(3))
+        self.assertTrue(primes.is_prime(2))
 
     def test_four_is_not_prime(self):
-        self.assertTrue(primes.is_prime(3))
+        self.assertTrue(primes.is_prime(2))
 
 
 if __name__ == '__main__':
